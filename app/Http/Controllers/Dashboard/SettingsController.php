5df6a2b6078fa7bc5c1a7e3f4bbab69f31fa5e6d
Mahmoud Badr <mahmoud.hussiba@gmail.com>
2022-10-21 10:12:33 +0200

    End Shipping branch

--- app/Http/Controllers/Dashboard/SettingsController.php
+++ app/Http/Controllers/Dashboard/SettingsController.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ShippingRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SettingsController extends Controller
 {
     public function editShippingMethod($type){
-       // shipping methods are free , inner , outer
+        // free , inner , outer for shipping methods
         switch ($type){
             case 'free': {
               $shippingMethod = Setting::where('key','free_shipping_label')->first();
@@ -33,15 +34,20 @@ class SettingsController extends Controller
     public function updateShippingMethod(ShippingRequest $request, $id)
     {
         try {
-
             $shipping_method = Setting::find($id);
+
+            DB::beginTransaction();
             $shipping_method-> update(['plain_value'=>$request->plain_value,]);
+
+            // save translation
             $shipping_method-> value = $request-> value;
             $shipping_method->save();
 
+            DB::commit();
             return redirect()->back()->with(['success'=>'تم التحديث بنجاح']);
         }catch (\Exception $ex){
-
+            return redirect()->back()->with(['error'=>'هناك خطأ يرجى المحاوله فيما بعد']);
+            DB::rollBack();
         }
 
     }
